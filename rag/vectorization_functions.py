ba397b4fb505863bcf2adac32c8465adcd0eeed2
w3680 <tw.w3680@gmail.com>
2025-04-09 13:24:57 +0200

    fixed prompt bug and blanked global

--- rag/vectorization_functions.py
+++ rag/vectorization_functions.py
@@ -4,7 +4,6 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from helper_functions.llm_base_client import llm_base_client_init
 
 def split_documents(documents, chunk_size=800, chunk_overlap=80): # check chunk size and overlap for our purpose
     """
@@ -42,11 +41,11 @@ def create_embedding_vector_db(chunks):
         model_name='sentence-transformers/all-MiniLM-L6-v2' # EMBEDDING MODEL! converts text to vector ( stick to it)
     )
     # create the vector store 
-    vector_db = FAISS.from_documents( # stores embeddings # from_documents includes metadata
+    vector_database = FAISS.from_documents( # stores embeddings # from_documents includes metadata
         documents=chunks,
         embedding=embedding
     )
-    return vector_db # optimize
+    return vector_database # optimize
 
 # Function to query the vector database and interact with Groq
 def query_vector_db(query, vector_db):
